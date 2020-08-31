837f3680de439b6b32b71ca1ab371948a60886ff
josielvs <josielvs@gmail.com>
2020-08-30 23:41:05 -0300

    Load exercises five and six!

---
+++ beginner/bloco_4/day_1/exercise5.js
@@ -0,0 +1,11 @@
+let degreeAngleA = 65;
+let degreeAngleB = 101;
+let degreeAngleC = 15;
+
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+if (sumOfAngles === 180) {
+  console.log('Ângulos válidos!');
+} else {
+  console.log('Ângulos inválidos!');
+};
