9018018feb0a7224ebbc1cd98554806875f30e96
josielvs <josielvs@gmail.com>
2020-09-29 11:23:04 -0300

    Exercicios do dia

---
+++ beginner/bloco_7/day_1/templateLiteral.js
@@ -0,0 +1,28 @@
+const array = ["JavaScript", "Shell Script", "Learn"]
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    )
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`)
+
+    result = `
+    ${result}
+
+    #goTrybe
+    `
+
+    return result
+}
+
+console.log(buildSkillsPhrase("Josiel"))
